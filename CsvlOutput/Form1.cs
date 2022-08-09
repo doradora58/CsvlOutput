2a08a9581f17f0a22153ea352f176a748bbacb9a
doradora58 <koudai58@gmail.com>
2022-08-09 09:15:24 +0900

    aaa

--- CsvlOutput/Form1.cs
+++ CsvlOutput/Form1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Data;
+using System.Data.Odbc;
 using System.IO;
 using System.Text;
 
@@ -22,23 +24,58 @@ namespace CsvlOutput
             {
                 // ストップウォッチクラス生成
                 var sw1 = new System.Diagnostics.Stopwatch();
-                // 時間計測開始
-                sw1.Start();
-
-                // ********時間計測したい処理Aを記述*****
-
-                // 計測終了
-                sw1.Stop();
-
                 // ストップウォッチクラス生成
                 var sw2 = new System.Diagnostics.Stopwatch();
-                // 時間計測開始
-                sw2.Start();
-
                 // ********時間計測したい処理Bを記述*****
+                // ********時間計測したい処理Aを記述*****
 
-                // 計測終了
-                sw2.Stop();
+                //接続文字列
+                string connectionString = @"DSN=TestCSV;";
+                var dataTable = new DataTable();
+                try
+                {
+                    using (var connection = new OdbcConnection(connectionString))
+                    {
+                        // 時間計測開始
+                        sw1.Start();
+                        connection.Open();
+                        // 計測終了
+                        sw1.Stop();
+
+                        var sql = "SELECT * FROM Test.csv WHERE B='i' AND C='u'" +
+                            "";
+                        using (var commnad = new OdbcCommand(sql, connection))
+                        {
+                            using (var adapter = new OdbcDataAdapter(commnad))
+                            {
+                                // 時間計測開始
+                                sw2.Start();
+                                adapter.Fill(dataTable);
+                                // 計測終了
+                                sw2.Stop();
+                            }
+                        }
+                    }
+                    //DataTableはforeachできないので、AsEnumerableを使う
+                    foreach (var row in dataTable.AsEnumerable())
+                    {
+                        //DataRow["列名"]で値を取得
+                        var char1 = row["A"];
+                        var char2 = row["B"];
+                        var char3 = row["C"];
+                        var char4 = row["D"];
+                        var char5 = row["E"];
+                        //Console.WriteLine($"{name} {age}歳 {bloodType}型");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+
 
                 // テキストに結果を書き出し
                 text1 += sw1.ElapsedMilliseconds.ToString() + ",";
